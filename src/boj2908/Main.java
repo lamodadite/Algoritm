6a0e26d221b4a616f85b3107fae69584c97bdb87
이종민 <102534186+lamodadite@users.noreply.github.com>
2022-12-08 14:36:14 +0900

    initial commit

--- src/boj2908/Main.java
+++ src/boj2908/Main.java
@@ -5,14 +5,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        StringBuffer sb1 = new StringBuffer();
-        StringBuffer sb2 = new StringBuffer();
-
-        String n1 = sc.next();
-        String n2 = sc.next();
-
-        sb1.append(n1);
-        sb2.append(n2);
+        StringBuffer sb1 = new StringBuffer(sc.next());
+        StringBuffer sb2 = new StringBuffer(sc.next());
 
         int num1 = Integer.parseInt(sb1.reverse().toString());
         int num2 = Integer.parseInt(sb2.reverse().toString());

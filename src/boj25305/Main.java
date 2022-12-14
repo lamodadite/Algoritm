da63765c32c5016888ca43fe8f59c1b76129c7e6
이종민 <102534186+lamodadite@users.noreply.github.com>
2022-12-14 15:54:21 +0900

    initial commit

---
+++ src/boj25305/Main.java
@@ -0,0 +1,39 @@
+package boj25305;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt(), k = sc.nextInt();
+
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        insertSort(arr);
+
+        System.out.println(arr[k-1]);
+    }
+
+    private static void insertSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int target = i;
+            while (target > 0) {
+                if (arr[target] > arr[target - 1]) {
+                    swap(arr, target, target - 1);
+                }
+                target--;
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}

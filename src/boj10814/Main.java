a824a2a03a5ed4de67157f0199fcd62ad2efd235
이종민 <102534186+lamodadite@users.noreply.github.com>
2022-12-18 18:12:28 +0900

    initial commit

--- src/boj10814/Main.java
+++ src/boj10814/Main.java
@@ -1,12 +1,21 @@
 package boj10814;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int N = Integer.parseInt(sc.nextLine());
 
+        List<String> list = new ArrayList<>();
 
+        for (int i = 0; i < N; i++) {
+            list.add(sc.nextLine());
+        }
+        sc.close();
 
+        Collections.sort(list, Comparator.comparingInt(o -> Integer.parseInt(o.split(" ")[0])));
+
+        list.forEach(System.out::println);
     }
 }

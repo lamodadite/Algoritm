d14dcf9ce5b07c3c5dbc7bd329ab7a56379487a1
이종민 <102534186+lamodadite@users.noreply.github.com>
2022-12-22 04:40:58 +0900

    initial commit

---
+++ src/boj2684/Main.java
@@ -0,0 +1,27 @@
+package boj2684;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final String[] coinSeq = {"TTT", "TTH", "THT", "THH", "HTT", "HTH", "HHT", "HHH"};
+
+        int n = sc.nextInt();
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            String sequence = sc.next();
+
+            for (String x : coinSeq) {
+                cnt = 0;
+                for (int j = 0; j < sequence.length() - 2; j++) {
+                    if (x.equals(sequence.substring(j, j + 3))) {
+                        cnt++;
+                    }
+                }
+                System.out.print(cnt + " ");
+            }
+            System.out.println();
+        }
+    }
+}

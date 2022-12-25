a82cb5ed754bc60dffa6d3faa8d30f605a9c1502
이종민 <102534186+lamodadite@users.noreply.github.com>
2022-12-25 16:47:43 +0900

    initial commit

---
+++ src/boj3053/Main.java
@@ -0,0 +1,15 @@
+package boj3053;
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double r = sc.nextDouble();
+
+        System.out.println(r*r*Math.PI);
+        System.out.println(r*r*2);
+    }
+}

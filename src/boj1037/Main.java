e447cd2281eff7cb31b9348026474a12b4a0821f
이종민 <102534186+lamodadite@users.noreply.github.com>
2022-12-25 21:50:43 +0900

    initial commit

---
+++ src/boj1037/Main.java
@@ -0,0 +1,24 @@
+package boj1037;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int x;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            x = sc.nextInt();
+            list.add(x);
+        }
+        Collections.sort(list);
+
+        System.out.println(list.get(0) * list.get(list.size()-1));
+    }
+}

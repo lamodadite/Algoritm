46dde0f68992c774b7a53c02cd571f8809c0d45c
이종민 <102534186+lamodadite@users.noreply.github.com>
2022-12-26 19:09:23 +0900

    initial commit

---
+++ src/boj9375/Main.java
@@ -0,0 +1,28 @@
+package boj9375;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            int n = Integer.parseInt(br.readLine());
+            HashMap<String, Integer> map = new HashMap<>();
+            for (int j = 0; j < n; j++) {
+                StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+                st.nextToken();
+                String cloth = st.nextToken();
+                map.put(cloth, map.getOrDefault(cloth, 0) + 1);
+            }
+            System.out.println(map.values().stream().mapToInt(x -> x + 1).reduce((x, y) -> x * y).getAsInt() - 1);
+            ;
+        }
+    }
+}

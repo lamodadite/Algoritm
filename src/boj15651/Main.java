c229c1f820d4e140c3295594d67cb7dace7cabf6
이종민 <102534186+lamodadite@users.noreply.github.com>
2022-12-28 15:55:55 +0900

    initial commit

---
+++ src/boj15651/Main.java
@@ -0,0 +1,37 @@
+package boj15651;
+
+import java.util.Scanner;
+
+public class Main {
+    static int[] arr;
+    static int N, M;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        arr = new int[M];
+
+        dfs(0);
+
+        System.out.println(sb);
+    }
+
+    public static void dfs(int depth) {
+        if (depth == M) {
+            for (int x : arr) {
+                sb.append(x).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            arr[depth] = i + 1;
+            dfs(depth + 1);
+        }
+    }
+}

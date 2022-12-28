802e0860025d7cfb46360ed00eabe433c2417253
이종민 <102534186+lamodadite@users.noreply.github.com>
2022-12-28 13:27:48 +0900

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,40 +2,59 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static String[][] arr;
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        int[] arr = {3, 5, 1, 7, 3, 5, 4, 2, 3, 7, 9, 5 ,3};
 
-        int N = sc.nextInt();
-        arr = new String[N][N];
-        for (int i = 0; i < N; i++) {
-            Arrays.fill(arr[i], " ");
-        }
+//        selection_sort(arr);
+//        bubble_sort(arr);
+        insertion_sort(arr);
 
-        starRecursion(0, 0, N);
-        StringBuilder sb = new StringBuilder();
-        for (String[] strings : arr) {
-            for (String string : strings) {
-                sb.append(string);
-            }
-            sb.append("\n");
-        }
-        System.out.println(sb);
+        System.out.println(Arrays.toString(arr));
     }
 
-    private static void starRecursion(int x, int y, int N) {
-        if (N == 1) {
-            arr[x][y] = "*";
-            return;
+    static void selection_sort(int[] arr) {
+        int min = Integer.MAX_VALUE;
+        int minIdx = -1;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (min > arr[j]) {
+                    min = arr[j];
+                    minIdx = j;
+                }
+            }
+            if (arr[i] > min) {
+                swap(arr, i, minIdx);
+            }
+            min = Integer.MAX_VALUE;
         }
+    }
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (!(i == 1 && j == 1)) {
-                    starRecursion(x + i*(N/3), y + j*(N/3), N/3);
+    static void bubble_sort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
                 }
             }
         }
+    }
+
+    static void insertion_sort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int x, int y) {
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
     }
 
 }
---
+++ src/boj2004/Main.java
@@ -0,0 +1,39 @@
+package boj2004;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+        long m = sc.nextLong();
+
+        long five = five_power(n) - five_power(m) - five_power(n - m);
+        long two = two_power(n) - two_power(m) - two_power(n - m);
+
+        System.out.println(Math.min(five, two));
+    }
+
+    static long five_power(long num) {
+        int count = 0;
+
+        while (num >= 5) {
+            count += num / 5;
+            num /= 5;
+        }
+
+        return count;
+    }
+
+    static long two_power(long num) {
+        int count = 0;
+
+        while (num >= 2) {
+            count += num / 2;
+            num /= 2;
+        }
+
+        return count;
+    }
+}

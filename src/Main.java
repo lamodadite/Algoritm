e391363832f7dc1bbe04a3ceebc769ce4f21aaac
이종민 <102534186+lamodadite@users.noreply.github.com>
2023-01-18 14:56:57 +0900

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,25 @@ public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-
-        for (int j = 1; j <= N; j++) {
-            for(int i = N - 1; i >= j; i--) {
-                System.out.print(" ");
-            }
-
-            for(int i = 1; i <= j; i++) {
-                System.out.print(i);
-            }
-            System.out.println();
+        int n = Integer.parseInt(br.readLine());
+        String[] strings = br.readLine().split(" ");
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+        dp[0] = 1;
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(strings[i]);
         }
+
+        for (int i = 1; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        System.out.println(Arrays.stream(dp).max().getAsInt());
+
     }
 }

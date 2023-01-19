8ef27a806b6a094e3daa41e28a4d180a7bdd985c
이종민 <102534186+lamodadite@users.noreply.github.com>
2023-01-19 16:57:16 +0900

    initial commit

---
+++ src/boj2805/Main.java
@@ -0,0 +1,44 @@
+package boj2805;
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static int[] trees;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); // 나무의 수
+        int m = sc.nextInt(); // 집으로 가져가려는 나무의 길이
+        trees = new int[n];
+
+        long max = 0;
+        for (int i = 0; i < n; i++) {
+            trees[i] = sc.nextInt();
+            if (max < trees[i]) {
+                max = trees[i];
+            }
+        }
+
+        long min = 0;
+
+        while (min < max) {
+
+            long mid = min + (max - min) / 2;
+            long length = 0;
+            for (int tree : trees) {
+                if (tree > mid) {
+                    length += tree - mid;
+                }
+            }
+
+            if (length < m) {
+                max = mid;
+            } else {
+                min = mid + 1;
+            }
+        }
+
+        System.out.println(min - 1);
+    }
+}

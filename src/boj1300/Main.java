83727c74ef41c76b309cd38df81bf669d541c1cd
이종민 <102534186+lamodadite@users.noreply.github.com>
2023-01-20 21:48:26 +0900

    initial commit

---
+++ src/boj1300/Main.java
@@ -0,0 +1,33 @@
+package boj1300;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int k = sc.nextInt();
+
+        long lo = 1;
+        long hi = k;
+
+        while (lo < hi) {
+
+            long mid = lo + (hi - lo) / 2;
+            long cnt = 0;
+
+            for (int i = 1; i <= N; i++) {
+                cnt += Math.min(N, mid / i);
+            }
+
+            if (k <= cnt) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+
+        System.out.println(lo);
+    }
+}

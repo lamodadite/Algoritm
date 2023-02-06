a07310d92f192bf5387ce01372388d00b7b2772c
이종민 <102534186+lamodadite@users.noreply.github.com>
2023-02-06 13:53:37 +0900

    initial commit

--- src/BOJ/boj2869/Main.java
+++ src/BOJ/boj2869/Main.java
@@ -1,13 +1,21 @@
 package BOJ.boj2869;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int A = sc.nextInt(), B = sc.nextInt(), V = sc.nextInt();
 
-        System.out.println((V-A)/(A-B) + 1);
+        int x = (V - A) / (A - B) + 1;
+        if ((V - A) % (A - B) == 0) {
+            System.out.println(x - 1);
+        } else {
+            System.out.println(x);
+        }
     }
 }

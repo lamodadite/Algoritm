9875f61113769bbe7f03ffa0585e5c391a407339
이종민 <102534186+lamodadite@users.noreply.github.com>
2023-03-01 16:35:55 +0900

    initial commit

--- src/BOJ/boj1991/Main.java
+++ src/BOJ/boj1991/Main.java
@@ -1,27 +1,72 @@
 package BOJ.boj1991;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+    static Node head = new Node('A', null, null);
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        String[] tree = new String[n * 3];
 
         sc.nextLine();
-        String parent, left, right;
         for (int i = 0; i < n; i++) {
             String[] node = sc.nextLine().split(" ");
-            parent = node[0];
-            left = node[1];
-            right = node[2];
-            int order = (int) parent.charAt(0) - (int) 'A';
-            tree[order] = parent;
-            tree[order * 2 + 1] = left;
-            tree[order * 2 + 2] = right;
+            char parent = node[0].charAt(0);
+            char left = node[1].charAt(0);
+            char right = node[2].charAt(0);
+            insertNode(head, parent, left, right);
         }
 
-        System.out.println(-1);
+        preOrder(head); // 전위순회
+        System.out.println();
+        inOrder(head); // 중위순회
+        System.out.println();
+        postOrder(head); // 후위순회
+        System.out.println();
+    }
+
+    private static void insertNode(Node temp, char parent, char left, char right) {
+        if (temp.value == parent) {
+            temp.left = (left == '.' ? null : new Node(left, null, null));
+            temp.right = (left == '.' ? null : new Node(right, null, null));
+        } else {
+            if(temp.left != null) insertNode(temp.left, parent, left, right);
+            if(temp.right != null) insertNode(temp.right, parent, left, right);
+        }
+    }
+
+    public static void preOrder(Node node) {
+        if(node ==null) return;
+        System.out.print(node.value);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public static void inOrder(Node node) {
+        if(node ==null) return;
+        inOrder(node.left);
+        System.out.print(node.value);
+        inOrder(node.right);
+    }
+
+    public static void postOrder(Node node) {
+        if(node ==null) return;
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.value);
+    }
+
+    static class Node {
+        char value;
+        Node left;
+        Node right;
+
+        public Node(char value, Node left, Node right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
     }
 }

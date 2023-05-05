3821c43d59e641f0b11b060550d8e9af3565870d
이종민 <102534186+lamodadite@users.noreply.github.com>
2023-05-05 21:44:08 +0900

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -5,38 +5,67 @@ import static java.util.stream.Collectors.toList;
 
 class Solution {
 
-    static boolean[] primes;
+    static class Node {
+        int to;
+        int weight;
 
-    public static void main(String[] args) {
-
-        int N = 10000;
-
-        make_primes(N);
-
-
-        System.out.println(primes[12]);
-
-
-
+        public Node(int to, int weight) {
+            this.to = to;
+            this.weight = weight;
+        }
     }
 
-    private static void make_primes(int n) {
-        primes = new boolean[n + 1];
+    public static void dijkstra(int v, int[][] data, int start) {
+        ArrayList<ArrayList<Node>> graph = new ArrayList<>();
 
-        if (n < 2) {
-            return;
+        for (int i = 0; i < v + 1; i++) {
+            graph.add(new ArrayList<>());
         }
 
-        primes[0] = primes[1] = true;
+        for (int i = 0; i < data.length; i++) {
+            graph.get(data[i][0]).add(new Node(data[i][1], data[i][2]));
+        }
 
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (primes[i]) continue;
-            for (int j = i * i; j < primes.length; j = j + i) {
-                primes[j] = true;
+        int[] dist = new int[v + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[start] = 0;
+
+        boolean[] visited = new boolean[v + 1];
+
+        PriorityQueue<Node> pq = new PriorityQueue<>((x, y) -> x.weight - y.weight);
+        pq.add(new Node(start, 0));
+
+        while (!pq.isEmpty()) {
+            Node curNode = pq.poll();
+
+            if (dist[curNode.to] < curNode.weight) {
+                continue;
+            }
+
+            for (int i = 0; i < graph.get(curNode.to).size(); i++) {
+                Node adjNode = graph.get(curNode.to).get(i);
+                if (dist[adjNode.to] > curNode.weight + adjNode.weight) {
+                    dist[adjNode.to] = curNode.weight + adjNode.weight;
+                    pq.add(new Node(adjNode.to, dist[adjNode.to]));
+                }
+            }
+
+        }
+
+        for (int i = 1; i < v + 1; i++) {
+            if (dist[i] == Integer.MAX_VALUE) {
+                System.out.println("INF ");
+            } else {
+                System.out.println(dist[i] + " ");
             }
         }
 
+    }
 
+
+    public static void main(String[] args) {
+        int[][] data = {{1, 2, 2}, {1, 3, 3}, {2, 3, 4}, {2, 4, 5}, {3, 4, 6}, {5, 1, 1}};
+        dijkstra(5, data, 1);
     }
 
 }

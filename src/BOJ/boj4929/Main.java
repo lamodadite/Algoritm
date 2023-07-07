39cd4e78050e534d4e7eef0beac685ca7c43bef5
이종민 <102534186+lamodadite@users.noreply.github.com>
2023-07-07 21:57:38 +0900

    initial commit

---
+++ src/BOJ/boj4929/Main.java
@@ -0,0 +1,15 @@
+package BOJ.boj4929;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String s1 = sc.nextLine();
+            String s2 = sc.nextLine();
+        }
+
+    }
+}
